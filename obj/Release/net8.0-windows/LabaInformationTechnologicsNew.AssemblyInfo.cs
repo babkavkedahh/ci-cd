19893c904d495be569b01a20cb213063aaae6241
babkavkedahh <SosiskaKIller2000@mail.ru>
2024-11-28 12:52:23 +0300

    test1

--- obj/Release/net8.0-windows/LabaInformationTechnologicsNew.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/LabaInformationTechnologicsNew.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("6578aa4e-4c82-462d-88be-fe7736653cb6")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabaInformationTechnologicsNew")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabaInformationTechnologicsNew")]
